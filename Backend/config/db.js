3af973335748e6716b7970cee73e9dccefbe09f4
Akshara <aksharaishu11@gmail.com>
2026-08-07 10:46:06 +0530

    Serve frontend with express

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,14 +1,29 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+    // Railway / Production
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    // Local Development
+    pool = new Pool({
+        user: "postgres",
+        host: "localhost",
+        database: "RentHub",
+        password: "2005",
+        port: 5432
+    });
+}
 
 pool.connect()
-.then(() => console.log("Database Connected"))
-.catch(err => console.log("Database Error:", err));
+    .then(() => console.log("✅ PostgreSQL Connected Successfully"))
+    .catch(err => console.log("❌ Database Connection Failed", err.message));
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const pool = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
@@ -8,10 +11,19 @@ const addressRoutes = require("./routes/addressRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 
 const app = express();
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
+
+
+// Serve Frontend
+app.use(express.static(path.join(__dirname, "../Frontend")));
+
+
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/address", addressRoutes);
@@ -19,10 +31,15 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/products", productRoutes);
 
 
+// Home page
 app.get("/", (req, res) => {
-    res.send("Welcome to RentHub Backend 🚀");
+    res.sendFile(
+        path.join(__dirname, "../Frontend/index.html")
+    );
 });
 
+
+// Database connection
 pool.connect()
 .then(() => {
     console.log("✅ PostgreSQL Connected Successfully");
@@ -32,6 +49,8 @@ pool.connect()
     console.log(err.message);
 });
 
+
+// Start Server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
